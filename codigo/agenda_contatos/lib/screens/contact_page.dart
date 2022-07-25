95a22a41d083ea63e750e1eb9c90b086a98962ce
Lindomar <lindomarfarineli@hotmail.com>
2022-07-25 17:26:56 -0300

    construcao da classe contactPage e solucao de bugs

---
+++ codigo/agenda_contatos/lib/screens/contact_page.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import '../helpers/contact_helper.dart';
+
+import 'package:image_picker/image_picker.dart';
+
+class ContactPage extends StatefulWidget {
+
+  /// um contato pode ou não ser recebido da tela principal
+  final Contact? contact;
+
+  /// construtor
+  const ContactPage({this.contact, Key? key}) : super(key: key);
+  //const ContactPage(this.contact ,{Key? key}) : super(key: key);
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+
+  ///controladores para os campos de texto. Por meio destes controladores
+  ///é possível editar os campos de texto
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  /// variáveis globais, a primeira utilizada para verificar se veio um contato
+  /// da outra página, a segunda é uma booleana para auxiliar no controle dos
+  /// campos de texto e terceira para ajudar com o foco, ou seja, onde o cursor
+  /// está no momento. Isto é interessante no processo de validação.
+  late Contact _editedContact;
+  bool _userEdited = false;
+  final _nameFocus = FocusNode();
+
+  /// chamado quando a página inicia.
+  @override
+  void initState() {
+    super.initState();
+    /// aqui é verificado se veio um contato, é uma forma que esta classe tem
+    /// de se comunicar com a classe anterior
+    if(widget.contact == null){
+      /// se o widget é nulo, então é um novo contato.
+      _editedContact = Contact();
+    }else {
+      /// caso contrário não é um novo contato, mas edição de um contato já
+      /// existente.
+      _editedContact = widget.contact!;
+
+      /// nesse caso, espera-se que os textos que vieram sejam mostrados, para
+      /// isso serve este trecho.
+      _nameController.text = _editedContact.name!;
+      _emailController.text = _editedContact.email!;
+      _phoneController.text = _editedContact.phone!;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    /// este widget impede que a página seja fechada automaticamente por meio
+    /// da seta voltar - há um tratamento mais abaixo.
+    return WillPopScope (
+      onWillPop: () => _requestPop() ,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(_editedContact.name ?? 'Novo Contato'),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            /// caso não haja um nome, o foco, ou seja, o cursor vai para o
+            /// campo nome, em vez de salvar o contato
+            if(_editedContact.name != null && _editedContact.name!.isNotEmpty){
+              Navigator.pop(context, _editedContact);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          child: const Icon(Icons.save),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _editedContact.img != null?
+                      FileImage(File(_editedContact.img!)):
+                      const AssetImage('images/person.png') as ImageProvider
+                    ),
+                  ),
+                ),
+                onTap: (){
+                  /// abre a câmera e salva o caminho da foto tirada no banco
+                  ImagePicker().pickImage(source: ImageSource.camera).
+                  then((file){
+                    if(file == null){
+                    return;
+                  }
+                  setState(() {
+                    _editedContact.img = file.path;
+                  });
+                  });
+                },
+              ),
+              TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: const InputDecoration(labelText: 'Nome'),
+                onChanged: (text){
+                  _userEdited = true;
+                  setState(() {
+                    _editedContact.name = text;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                onChanged: (text){
+                  _userEdited = true;
+                  _editedContact.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: const InputDecoration(labelText: 'Phone'),
+                onChanged: (text){
+                  _userEdited = true;
+                  _editedContact.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// esta função apenas verifica se houve edição ou não de algum dado, caso
+  /// tenha havido edição, é perguntado se deseja descartar.
+  Future<bool> _requestPop(){
+    if(_userEdited){
+      showDialog(context: context, builder:(context){
+        return AlertDialog(
+          title: const Text('Descartar Alterações'),
+          content: const Text('Se sair as alterações serão perdidas'),
+          actions: [
+            TextButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: const Text('Cancelar')),
+           TextButton(onPressed: (){
+              Navigator.pop(context);
+              Navigator.pop(context);
+            }, child: const Text('Sim')),
+          ]
+        );
+      });
+      return Future.value(false);
+    }else{
+      return Future.value(true);
+    }
+  }
+
+}
--- codigo/agenda_contatos/lib/screens/home_page.dart
+++ codigo/agenda_contatos/lib/screens/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io';
+
+import 'contact_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,9 +25,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    setState(() {
-      helper.getAllContacts().then((list) =>  contacts = list );
-    });
+    _getAllContacts();
   }
 
   @override
@@ -36,7 +38,9 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          _showContactPage();
+        },
         child: const Icon(Icons.add),
         backgroundColor: Colors.red,
       ),
@@ -44,9 +48,129 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.all(10),
           itemCount: contacts.length,
           itemBuilder: (context, index){
-
+            return _contactCard(context, index);
           },
     ),
     );
   }
+
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: contacts[index].img != null? 
+                          FileImage(File(contacts[index].img!)):
+                          const AssetImage("images/person.png") as ImageProvider
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(contacts[index].name ?? '', style: const TextStyle(fontSize:
+                    22, fontWeight: FontWeight.bold),),
+                    Text(contacts[index].email ?? '', style: const TextStyle(fontSize:
+                    18),),
+                    Text(contacts[index].phone ?? '', style: const TextStyle(fontSize:
+                    18),),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      onTap: (){
+        _showOptions(context, index);
+      },
+    );
+  }
+
+  /// função que cria alguns botões quando se clica no contato, para alterar,
+  /// excluir ou ligar se necessário.
+  void _showOptions(BuildContext context, int index){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return BottomSheet(
+            onClosing: (){},
+            builder: (context){
+              return Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.all(10),
+                    child: TextButton(
+                      child: const Text('Ligar', style: TextStyle(color: Colors.red,
+                      fontSize: 20),),
+                      onPressed: () {
+                        launchUrl(Uri(scheme: 'tel', path: contacts[index].phone));
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ),
+                    Padding(padding: const EdgeInsets.all(10),
+                    child: TextButton(
+                      child: const Text('Editar', style: TextStyle(color: Colors.red,
+                      fontSize: 20),),
+                      onPressed: (){
+                        Navigator.pop(context);
+                        _showContactPage(contact: contacts[index]);
+                      },
+                    ),),
+                    Padding(padding: const EdgeInsets.all(10),
+                    child: TextButton(
+                      child: const Text('Excluir', style: TextStyle(color: Colors.red,
+                      fontSize: 20),),
+                      onPressed:(){
+                        helper.deleteContact(contacts[index].id!);
+                        setState(() {
+                          contacts.removeAt(index);
+                          Navigator.pop(context);
+                        });
+                      },
+                    ),),
+                  ],
+                ),
+              );
+            }
+          );
+        });
+  }
+
+  /// salva ou atualiza o retorno de contactPage, de acordo com a necessidade
+  void _showContactPage({Contact? contact}) async{
+     final recContact = await Navigator.push(context,
+    MaterialPageRoute(builder: (context) => ContactPage(contact: contact)),
+    );
+    if(recContact != null ){
+      if(contact != null){
+        await helper.updateContact(recContact);
+      }else {
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
+  }
+
+  /// atualiza a lista de contatos
+  void _getAllContacts(){
+    setState(() {
+      helper.getAllContacts().then((list) =>  contacts = list );
+    });
+  }
+
 }
